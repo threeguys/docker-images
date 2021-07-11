216fb518f13b4e982120c74348f1b4f4ffc485d8
threeguys <4969340+threeguys@users.noreply.github.com>
2021-07-10 22:06:13 -0700

    Removing extraneous stuff due to lack of nodejs knowledge
    I have no idea what I'm doing so I did more stuff and made it
    more complicated by have modules in the root directory. Additionally,
    it made it where only root could run, which kind of sucks since I
    want to write files out as the current user.

--- eleventy/entrypoint.sh
+++ eleventy/entrypoint.sh
@@ -3,15 +3,12 @@
 INPUT=${INPUT:-/src}
 OUTPUT=${OUTPUT:-/site}
 
-export PATH=/root/node_modules/bin:${PATH}
-export NPM_CONFIG_PREFIX=/root/node_modules
-
 case "$1" in
     sh|bash|/bin/bash|/bin/sh)
         set -- "$@"
     ;;
     *)
-        set -- /root/node_modules/bin/eleventy --input=${INPUT} --output=${OUTPUT} "$@"
+        set -- /usr/local/bin/eleventy --input=${INPUT} --output=${OUTPUT} "$@"
     ;;
 esac
 
